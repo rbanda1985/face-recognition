828c51f8cab6596b4bbd04f23a207f942fb32fd3
rbanda1985 <robertbanda85@gmail.com>
2023-02-19 22:19:26 -0600

    Add: Register, Signin, Signout functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,110 @@
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Clarifai from 'clarifai';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '2930ba8fd4144ec182265dc6ca72fb65'
+});
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height)
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({
+      imageUrl: this.state.input
+    })
+    app.models
+      .predict(
+        {
+          id: 'face-detection',
+          name: 'face-detection',
+          version: '6dc7e46bc9124c5c8824be4822abe105',
+          type: 'visual-detector',
+        }, this.state.input)
+        .then(response => {
+         this.displayFaceBox(this.calculateFaceLocation(response))
+        }).catch(err => {
+          console.log(err);
+        })
+  }
+
+  onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if(route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({
+      route: route,
+    });
+  }
+
+  render(){
+    const { isSignedIn, box, imageUrl, route } = this.state;
+    return(
+        <div className="App">
+          <ParticlesBg type="cobweb" bg={true} />
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+          { route === 'home' 
+          ? 
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+          : (
+            route === 'signin' 
+            ? <SignIn onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange}/>
+          )
+          
+        }
+        </div>
+      )
+  }
 }
 
 export default App;
