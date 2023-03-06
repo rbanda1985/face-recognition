fede446b9a6f4a96d9265c5e84476a1536f7cb87
rbanda1985 <robertbanda85@gmail.com>
2023-03-06 16:43:12 -0600

    Added port number inside of url on route

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response => {
           if(response){
-            fetch('https://face-recognition-yoih.onrender.com/image', {
+            fetch('https://face-recognition-yoih.onrender.com:3000/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
