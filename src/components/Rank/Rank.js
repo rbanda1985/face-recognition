813122f7708e6e573bb7688a6337eec208af0881
rbanda1985 <robertbanda85@gmail.com>
2023-03-11 22:26:03 -0600

    fix

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -7,7 +7,7 @@ const Rank = ({ name, entries }) => {
         {`${name}, your current rank is...`}
       </div>
       <div className='white f1'>
-        {{entries}}
+        {entries}
       </div>
     </div>
   )
